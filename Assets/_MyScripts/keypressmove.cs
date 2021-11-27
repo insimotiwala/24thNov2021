ffce0361a75e2359e5333a563c151898988f0772
insimotiwala <86950756+insimotiwala@users.noreply.github.com>
2021-11-26 22:53:05 -0800

    joystick working

--- Assets/_MyScripts/keypressmove.cs
+++ Assets/_MyScripts/keypressmove.cs
@@ -8,6 +8,9 @@ public class keypressmove : MonoBehaviour
 {
     // public GameObject part;
     //public GameObject[] Roboparts;
+    public float speed = 10f;
+
+    private RectTransform rect;
 
     //public List<GameObject> Parts = new List<GameObject>();
     public List<GameObject> Roboparts = new List<GameObject>();
@@ -18,6 +21,10 @@ public class keypressmove : MonoBehaviour
     private void Start()
 
     {
+        {
+            rect = GetComponent<RectTransform>();
+        }
+
         foreach (GameObject p in Roboparts)
         {
             p.GetComponent<MobileUnit>().enabled = false;
@@ -28,6 +35,7 @@ public class keypressmove : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        JoystickMovement();
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(0.5f, 0f, 0f);
@@ -60,5 +68,28 @@ public class keypressmove : MonoBehaviour
         col.GetComponent<CollisionDetection>().enabled = true;
         Roboparts.Add(col.gameObject);
         Debug.Log("hit");
+    }
+
+    private void JoystickMovement()
+    {
+        //get input
+        Vector3 joy = new Vector3(Input.GetAxis("RightJoyY"), 0, Input.GetAxis("RightJoyX"));
+
+        //camera vectors
+        Vector3 forward = Camera.main.transform.forward;
+        // Debug.DrawRay(transform.position, forward * 10, Color.green);
+        Vector3 project = Vector3.ProjectOnPlane(forward, Vector3.up);
+        // Debug.DrawRay(transform.position, project * 10, Color.blue);
+        Vector3 right = Camera.main.transform.right;
+        // Debug.DrawRay(transform.position, right * 10, Color.black);
+
+        //only continue if joystick pressed more than 0.3f
+        if (joy.magnitude < 0.3f) { return; }
+        Debug.Log("Player Move");
+
+        //move camera
+        Vector3 move = right * -joy.x + project * -joy.z;
+        transform.Translate(move.normalized * Time.deltaTime * speed);
+        //Debug.DrawRay(transform.position, move, Color.red);
     }
 }
